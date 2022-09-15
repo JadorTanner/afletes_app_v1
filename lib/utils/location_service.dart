ad52bd56119866c5ac8477426868e55477bd750e
jadortanner <tannerjador@gmail.com>
2022-09-15 16:05:38 -0400

    provider for location

--- lib/utils/location_service.dart
+++ lib/utils/location_service.dart
@@ -2,10 +2,25 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:afletes_app_v1/utils/constants.dart';
+import 'package:afletes_app_v1/utils/load_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart';
 
-class LocationService {
+class LocationService extends ChangeNotifier {
   final String key = Constants.googleMapKey;
+  static late BitmapDescriptor loadMarkerIcon, carMarkerIcon;
+
+  init() async {
+    loadMarkerIcon = BitmapDescriptor.fromBytes(
+      await getBytesFromAsset('assets/img/load-marker-icon.png', 70),
+    );
+
+    carMarkerIcon = BitmapDescriptor.fromBytes(
+      await getBytesFromAsset('assets/img/camion3.png', 5),
+    );
+  }
 
   Future<String> getPlaceId(String input) async {
     final String url =
@@ -34,4 +49,38 @@ class LocationService {
       return {};
     }
   }
+
+  //PARA EL PROVIDER
+  bool hasLocationPermission = false;
+  askLocationPermission() {
+    hasLocationPermission = true;
+    notifyListeners();
+  }
+
+  List<Marker> markers = [];
+  addMarker(Marker newMaker) {
+    markers.add(newMaker);
+    notifyListeners();
+  }
+
+  removeMarker(MarkerId markerId) {
+    markers.removeWhere((element) => element.markerId == markerId);
+    notifyListeners();
+  }
+
+  updateMarker(MarkerId markerId, Marker newMaker) {
+    removeMarker(markerId);
+    addMarker(newMaker);
+  }
+
+  clearMarkers() {
+    markers.clear();
+    notifyListeners();
+  }
+
+  //POLYLINES
+  List<LatLng> polylineCoordinates = [];
+  late PolylinePoints polylinePoints;
+
+  generatePolylines() {}
 }
